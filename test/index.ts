1f6f33f7fc6d3df5851926128f80269b680ecfa7
Philip Diffenderfer <pdiffenderfer@gmail.com>
2020-08-01 23:35:03 -0400

    List number of operations

--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,20 @@
 // import { describe, it, expect } from 'jest';
 
+import { defs } from '../src';
 
 // tslint:disable: no-magic-numbers
 
 describe('index', () => {
 
-  it('has test', () =>
+  it('count operations', () =>
   {
-    
+    let total = 0;
+
+    defs.typeList.forEach((type) => {
+      total += Object.keys(type.operations.map).length;
+    });
+
+    console.log('Total # of operations', total);
   });
 
 })
